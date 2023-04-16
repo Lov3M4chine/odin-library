c57726ef7a74122b0ac85058e7b0ccab2788efb8
Lov3M4chine <125694577+Lov3M4chine@users.noreply.github.com>
2023-04-16 10:10:42 -0600

    Add new book modal and search modal

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,32 @@
-function Book(title, author, isbn, pages, read ) {
-  this.title = title;
-  this.author = author;
-  this.isbn = isbn;
-  this.pages = pages;
-  this.read = read;
-}
+const newBookBtn = document.getElementById("new-book-btn");
+const newBookModal = document.getElementById("new-book-modal");
+const closeNewBookModal = document.getElementById("close-new-book-modal");
+const searchBtn = document.getElementById("search-btn");
+const searchModal = document.getElementById("search-modal")
+const manualBtn = document.getElementById("manual-btn");
+const closeSearchModal = document.getElementById("close-search-modal")
+const overlay = document.getElementById("overlay");
 
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-};
+newBookBtn.addEventListener("click", () => {
+  overlay.classList.remove("hidden");
+  newBookModal.classList.remove("hidden");
+});
 
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+closeNewBookModal.addEventListener("click", () => {
+  overlay.classList.add("hidden");
+  newBookModal.classList.add("hidden");
+});
+
+searchBtn.addEventListener("click", () => {
+  searchModal.classList.remove("hidden")
+  newBookModal.classList.add("hidden");
+});
+
+closeSearchModal.addEventListener("click", () => {
+  overlay.classList.add("hidden");
+  searchModal.classList.add("hidden");
+})
+
+manualBtn.addEventListener("click", () => {
+  // Handle manual button click event
+});
